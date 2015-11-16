8c8abbcc4e20186b08352ea96b7aa928a6aef10b
Omrika <omrikafolkes@gmail.com>
2015-11-16 15:30:47 -0500

    done

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,6 @@
-# Implement your procedural solution here!
+def sum_square_difference(number)
+  arr=*(1..number)
+  sum = arr.map { |x| (x**2)}.inject(:+)
+  diff = (arr.inject(:+)) ** 2
+  return diff - sum
+end
