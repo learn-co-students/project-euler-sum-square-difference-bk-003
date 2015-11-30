8c687714d75e3205235e292d317cf01c664b2d76
cloudleo <l.castillo1989@hotmail.com>
2015-11-29 20:09:17 -0500

    give me all of it

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,23 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference 
+def initialize(num)
+@num = num
+end
+
+def difference
+  var = []
+  bar = []
+  @num.times do |i|
+var << (i + 1)**2
+  end
+  bob = var.inject(:+)
+  
+  @num.times do |i|
+bar << (i+ 1)
+  end
+ foo = bar.inject(:+) 
+return foo**2 - bob
+end
+
+
+end
