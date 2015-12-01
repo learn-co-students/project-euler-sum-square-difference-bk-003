ab14aac37afcb39bab7dc990f2581f5a61901593
Ilapides <isaac.lapides@gmail.com>
2015-12-01 14:26:47 -0500

    done

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,21 @@
 # Implement your procedural solution here!
+require 'pry'
+def sum_square_difference(num)
+  return square_sum(num) - sum_square(num)
+end
+
+def sum_square(num)
+  sum = 0
+  num.times do |i|
+    sum += (i+1)**2
+  end
+  return sum
+end
+
+def square_sum(num)
+  sum = 0
+  num.times do |i|
+    sum += (i+1)
+  end
+  return sum**2
+end
