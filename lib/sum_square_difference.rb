a6359240c5c8ef7b2195f763d734a75536865394
Andrew Persad <akpersad@gmail.com>
2015-12-15 13:05:45 -0500

    done

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+def sum_square_difference(arg)
+	array1 = []
+	array2 = []
+	x = 0
+	while arg > x
+		array1 << arg**2
+		array2 << arg
+		arg -= 1
+	end
+
+	array1 = array1.inject{|sum,x| sum + x }
+	array2 = (array2.inject{|sum,x| sum + x })**2
+
+	return array2 - array1
+end
