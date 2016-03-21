0cbaade55e3d314056a39cd51354f04a90b6b045
Stephen Koltz <steve.koltz@gmail.com>
2016-03-21 16:16:27 -0400

    sum sums

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,7 @@
+
 # Implement your procedural solution here!
+def sum_square_difference(num)
+	sum = (1..num).inject {|x,y| x+y }
+	square = (1..num).inject {|x,y| x + y*y}
+	return (sum*sum - square)
+end
